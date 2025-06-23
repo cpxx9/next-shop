26891465ccbda2a5dd1062a78de0945e60957659
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-23 16:33:54 -0400

    feat: auth actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { signInFormSchema } from "@/lib/validators";
+import { signIn, signOut } from "@/auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+export async function signInWithCredentials(
+  previousState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    await signIn("credentials", user);
+    return { success: true, message: "Sign in success." };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: "Invalid credentials" };
+  }
+}
+
+export async function signOutUser() {
+  await signOut();
+}
