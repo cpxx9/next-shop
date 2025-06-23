413129e9ced0058698dfa4c30f43adbdc4d84370
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-23 15:25:26 -0400

    feat: seed users

--- db/seed.ts
+++ db/seed.ts
@@ -7,7 +7,27 @@ import sampleData from "@/db/sample-data";
     console.log("Database seeding failed...");
   });
 
+  await prisma.account.deleteMany().catch((e) => {
+    console.log("database seeding failed...");
+  });
+
+  await prisma.session.deleteMany().catch((e) => {
+    console.log("Database seeding failed...");
+  });
+
+  await prisma.verificationToken.deleteMany().catch((e) => {
+    console.log("Database seeding failed...");
+  });
+
+  await prisma.user.deleteMany().catch((e) => {
+    console.log("Database seeding failed...");
+  });
+
   await prisma.product.createMany({ data: sampleData.products }).catch((e) => {
+    console.log("Database seeding failed...");
+  });
+
+  await prisma.user.createMany({ data: sampleData.users }).catch((e) => {
     console.log("Database seeding failed...");
   });
 
