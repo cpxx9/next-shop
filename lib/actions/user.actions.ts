00dff2b1795769bbb9698ce41e29c74f89293c06
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-23 19:26:33 -0400

    feat: handle errors with better ux

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "@/lib/utils";
 
 export async function signInWithCredentials(
   previousState: unknown,
@@ -57,10 +58,14 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
 
     return { success: true, message: "User registered successfully." };
   } catch (error) {
+    console.log(error.name);
+    console.log(error.code);
+    console.log(error.errors);
+    console.log(error.meta?.target);
     if (isRedirectError(error)) {
       throw error;
     }
 
-    return { success: false, message: "User was not registered" };
+    return { success: false, message: formatError(error) };
   }
 }
