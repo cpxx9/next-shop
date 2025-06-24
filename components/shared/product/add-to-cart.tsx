295b12e632f5fab3506c841ea769213a92382f43
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-24 06:42:46 -0400

    feat: add to cart button hooks and onclick

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -5,16 +5,44 @@ import { Plus } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 import { CartItem } from "@/types";
+import { addItemToCart } from "@/lib/actions/cart.actions";
 
 interface PropTypes {
   item: CartItem;
 }
 
 const AddToCart = ({ item }: PropTypes) => {
-  const handleAddToCart = async () => {};
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+    if (!res.success) {
+      toast({
+        variant: "destructive",
+        description: res.message,
+      });
+      return;
+    }
+
+    toast({
+      description: `${item.name} added to cart`,
+      action: (
+        <ToastAction
+          className="bg-primary text-white hover:bg-gray-800"
+          altText="Go to Cart"
+          onClick={() => router.push("/cart")}
+        >
+          Go to Cart
+        </ToastAction>
+      ),
+    });
+  };
 
   return (
-    <Button className="w-full" type="button" onClick={handleAddToCart}></Button>
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      + Add to Cart
+    </Button>
   );
 };
 
