c14a52abbc0dcc7e89c828d688b6cadff1535246
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-24 21:31:00 -0400

    feat: start layout for shipping address page

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -4,6 +4,7 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
+import ShippingAddressForm from "@/app/(root)/shipping-address/shipping-address-form";
 
 export const metadata: Metadata = {
   title: "Shipping Address",
@@ -24,7 +25,7 @@ const ShippingAddressPage = async () => {
 
   return (
     <>
-      <h2>Address</h2>
+      <ShippingAddressForm address={user.address as ShippingAddress} />
     </>
   );
 };
---
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { shippingAddressSchema } from "@/lib/validators";
+import { ShippingAddress } from "@/types";
+import { useRouter } from "next/router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { shippingAddressDefaultValues } from "@/lib/constants";
+import { useTransition } from "react";
+
+interface PropTypes {
+  address: ShippingAddress;
+}
+
+const ShippingAddressForm = ({ address }: PropTypes) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof shippingAddressSchema>>({
+    resolver: zodResolver(shippingAddressSchema),
+    defaultValues: address || shippingAddressDefaultValues, // remove default values for prod
+  });
+
+  return (
+    <>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Shipping Address</h1>
+      </div>
+    </>
+  );
+};
+
+export default ShippingAddressForm;
