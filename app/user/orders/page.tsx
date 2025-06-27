91917ae27622c83437dcc92f6921046cd65ae4ca
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-26 21:23:45 -0400

    feat: start user orders/profile pages

---
+++ app/user/orders/page.tsx
@@ -0,0 +1,9 @@
+const OrdersPage = () => {
+  return (
+    <>
+      <h1>orders</h1>
+    </>
+  );
+};
+
+export default OrdersPage;
---
+++ app/user/profile/page.tsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+  return (
+    <>
+      <h2>profile</h2>
+    </>
+  );
+};
+
+export default ProfilePage;
