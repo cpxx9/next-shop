c314b4fd6447bf7d80a65264a8698862ce264fdc
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-27 07:20:36 -0400

    feat: display orders on user page

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,8 +1,83 @@
-const OrdersPage = () => {
+import { Metadata } from "next";
+import { getMyOrders } from "@/lib/actions/order.actions";
+import { formatCurrency, formatDateTime, shortenUuid } from "@/lib/utils";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+
+export const metadata: Metadata = {
+  title: "My Orders",
+};
+
+interface PropTypes {
+  searchParams: Promise<{ page: string }>;
+}
+
+const OrdersPage = async ({ searchParams }: PropTypes) => {
+  const { page } = await searchParams;
+  const orders = await getMyOrders({
+    page: Number(page) || 1,
+  });
+
+  console.log(orders);
+
   return (
-    <>
-      <h1>orders</h1>
-    </>
+    <div className="space-y-2 flex-1">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data?.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{shortenUuid(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt ? (
+                    <Badge variant="secondary">
+                      {formatDateTime(order.paidAt).dateOnly}
+                    </Badge>
+                  ) : (
+                    <Badge variant="destructive">Not Paid</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt ? (
+                    <p>{formatDateTime(order.deliveredAt).dateTime}</p>
+                  ) : (
+                    <Badge variant="destructive">Not Delivered</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  <Link href={`/order/${order.id}`}>
+                    <span className="px-2">Details</span>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   );
 };
 
--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -49,6 +49,16 @@ const UserButton = async () => {
               </div>
             </div>
           </DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/profile">
+              Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link className="w-full" href="/user/orders">
+              Order History
+            </Link>
+          </DropdownMenuItem>
           <DropdownMenuItem className="p-0 mb-1">
             <form action={signOutUser} className="w-full">
               <Button
