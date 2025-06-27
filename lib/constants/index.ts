96d4d8384350ad0081a9f696e5230da4b200e016
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-06-26 20:52:49 -0400

    feat: new title

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  "Modern e-Commerce platform built with Next.js";
+  "E-commerce store to test payments and NextJS";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT =
