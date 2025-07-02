7d541522f74893d33fbf08d65e69d7e4c1221ea3
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-07-02 09:17:07 -0400

    feat: actions for updating/creating products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,9 +2,11 @@
 import { prisma } from "@/db/prisma";
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "@/lib/constants";
 import { convertToPlainObject, formatError } from "@/lib/utils";
+import { insertProductSchema, updateProductSchema } from "@/lib/validators";
 import { Product } from "@/types";
 import { revalidatePath } from "next/cache";
 import { DO_NOT_USE_OR_YOU_WILL_BE_FIRED_EXPERIMENTAL_REACT_NODES } from "react";
+import z from "zod";
 
 // Get latest products
 export async function getLatestProducts() {
@@ -64,6 +66,46 @@ export async function deleteProduct(productId: string) {
     return {
       success: true,
       message: "Product deleted successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+export async function createProduct(data: z.infer<typeof insertProductSchema>) {
+  try {
+    const product = insertProductSchema.parse(data);
+    await prisma.product.create({ data: product });
+
+    revalidatePath("/admin/products");
+    return {
+      success: true,
+      message: "Product created successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
+  try {
+    const product = updateProductSchema.parse(data);
+    const productExists = await prisma.product.findFirst({
+      where: { id: product.id },
+    });
+    if (!productExists) throw new Error("Product not found");
+    await prisma.product.update({ where: { id: product.id }, data: product });
+
+    revalidatePath("/admin/products");
+    return {
+      success: true,
+      message: "Product updated successfully",
     };
   } catch (error) {
     return {
